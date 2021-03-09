48c420100de5aa02c2f39cd3fc3f64c9696e6244
晨逸轩 <l>
2021-03-09 16:46:29 +0800

    'nice'

---
+++ src/views/departments/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="departments-container">
+    <div class="app-container">
+      <h2>
+        组织结构
+      </h2>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/employees/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="dashboard-container">
+    <div class="app-container">
+      <h2>
+        员工
+      </h2>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  /* eslint-disable */
+  name: ' index '
+}
+</script>
+
+<style scoped>
+
+</style>
